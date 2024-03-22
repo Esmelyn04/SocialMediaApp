691e3b90197da22ac54abdf4f5ee4aa00cea26fd
Esmelyn04 <EML_JOSE_BAEZ@HOTMAIL.COM>
2024-03-22 12:05:56 -0400

    Passed props to Post component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import Post from './components/Post';
 
 function App(){
+
   return(
     <div>
       <h1>Fakebook!</h1>
+
       {/* CreatePostForm */}
+
       {/* Feed */}
-      <Post />
+
+      <Post content="This is a test post!" />
+      <Post content="This is another test post!" />
+
     </div>
     )
 }
